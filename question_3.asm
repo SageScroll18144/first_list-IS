68a675e561d07dc5b14de774db25beab87ac9b82
SageScroll18144 <ffss@cin.ufpe.br>
2023-06-10 15:59:41 -0300

    progresso

--- question_3.asm
+++ question_3.asm
@@ -1,11 +1,15 @@
 org 0x7c00
 jmp _start
 
+odd db "eh impar", 0
+even db "eh par", 0
+
 _start:
     xor ax, ax 
     xor bx, bx
     xor cx, cx 
     xor dx, dx 
+    xor si, si
 
     ;tela
     mov ah, 0
@@ -26,17 +30,17 @@ _start:
     call getchar
     call endl
 
-    ;W
-    xor ax, ax
-    call getchar
-    call endl
-
     ;Z
     xor ax, ax
     call getchar
     call endl
 
-    xor cx, cx
+    ;W
+    xor ax, ax
+    call getchar
+    call endl
+
+    xor si, si
     call solve
 
     call done
@@ -48,14 +52,14 @@ getchar:
     mov ah, 0x0
     int 16h
 
-    call putchar
-    
     sub ax, '0'
-
     stosb
-    inc si
-    
+    add ax, '0'
+
+    call putchar
+        
     ret
+
 
 putchar:
     mov ah, 0x0e
@@ -70,7 +74,91 @@ endl:
     ret
 
 solve:
-    ;(x*y) + (z*w) - (x/z)+(w/y),
+    xor si, si
+    
+    lodsb
+    mov bx, ax
+    
+    lodsb
+    imul bx, ax
+
+    lodsb
+    mov cx, ax
+
+    lodsb
+    imul cx, bx
+
+    add bx, cx
+
+    ;até aqui foram os dois primeiros literais
+
+    xor si, si
+    
+    lodsb
+    mov cx, ax
+    
+    lodsb
+    lodsb
+    idiv cx, ax
+
+    sub bx, cx
+
+    ;ate aqui foi o terceiro literal
+
+    xor si, si
+    
+    lodsb
+    lodsb
+    mov cx, ax
+
+    lodsb
+    lodsb
+    idiv ax, cx
+
+    add bx, ax
+
+    mov ax, bx
+    and ax, 1
+
+    cmp ax, 1
+    je .odd
+
+    mov si, even
+    call print_str
+
     ret
+
+    .odd:
+        mov si, odd
+        call print_str
+        ret
+
+
+print_str:
+    lodsb ; carrega uma letra em si
+    cmp al, 0
+    je .done
+
+    mov ah, 0eh
+    int 10h
+    jmp print_str
+
+    .done:
+        ret
+
+teste:
+    lodsb
+    
+    cmp al, 0
+    je .done
+
+    add ax, '0' 
+    call putchar
+
+    jmp teste
+    
+    .done:
+        ret
+
 times 510 - ($ - $$) db 0
 dw 0xaa55
