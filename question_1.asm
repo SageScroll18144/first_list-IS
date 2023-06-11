6fa7e2f7e002c689314fcd0096eef4818adaa944
SageScroll18144 <ffss@cin.ufpe.br>
2023-06-11 18:35:04 -0300

    prototipo da primeira questao

--- question_1.asm
+++ question_1.asm
@@ -1,30 +1,66 @@
 org 0x7c00
 jmp 0x0000:_start
 
-hello db "Felipe Santos eaw", 0
+flag db 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 7, 8, 8, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 8, 7, 8, 8, 8, 8, 0, 0, 0, 0, 8, 8, 0, 0, 0, 0, 8, 8, 8, 8, 3, 1, 8, 8, 8, 8, 1, 8, 0, 0, 0, 0, 0, 0, 8, 8, 1, 3, 9, 9, 8, 1, 9, 8, 0, 0, 0, 0, 0, 0, 8, 8, 9, 9, 15, 15, 9, 9, 9, 8, 0, 0, 0, 0, 8, 0, 8, 9, 9, 9, 9, 3, 9, 9, 9, 1, 0, 0, 0, 0, 8, 8, 8, 9, 15, 15, 15, 3, 9, 9, 9, 1, 0, 0, 0, 0, 8, 0, 8, 9, 9, 9, 15, 15, 9, 9, 3, 8, 0, 0, 0, 0, 8, 8, 8, 8, 8, 9, 9, 9, 9, 8, 8, 0, 0, 0, 0, 0, 8, 8, 8, 0, 0, 8, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0
 
 _start:
     xor ax, ax
+    xor cx, cx
+    xor dx, dx
     mov ds, ax
-    mov es, ax
 
-    mov si, hello
+    mov ah, 0
+    mov al, 13h
+    int 10h
 
-    call print_str
+    ; mov ah, 0x0
+    ; mov bh, 0
+    ; mov bl, 1h 
+    ; int 10h
+
+    mov si, flag
+    call print_flag
 
     jmp done
 
-print_str:
-    lodsb ; carrega uma letra em si
-    cmp al, 0
+print_flag:
+    ;;;;;;;;;
+
+    cmp cx, 16
     je .done
 
-    mov ah, 0eh
+    cmp dx, 16
+    je .next_step
+
+    lodsb
+    mov ah, 0ch
     int 10h
-    jmp print_str
+
+    inc dx
+    jmp print_flag
+
+    .next_step:
+        inc cx
+        xor dx, dx
+        call endl
 
     .done:
         ret
+
+endl: 
+    mov ax, 0x0a
+    call putchar
+    
+    mov ax, 0x0d
+    call putchar
+    
+    ret        
+	
+putchar:
+    mov ah, 0x0e
+    int 10h 
+    ret
+
 done:
     jmp $
 
